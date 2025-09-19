ee81fecf017d494cd7cb3376555a99ca905e4101
mamahda <gbrnmhd89@gmail.com>
2025-09-19 22:29:45 +0700

    ganti tema dan tambahin content

--- quiz1/pages/hometown.js
+++ quiz1/pages/hometown.js
@@ -1,8 +1,20 @@
 export default function Hometown() {
   return /*html*/ `
     <div class="hometown">
-      <h2>Welcome to the</h2>
-      <h1>Hometown Page</h1>
+      <div class="container">
+        <h2 class="text-center header">Ponorogo</h2>
+        <div class="content"> 
+          <p>Anim nulla minim esse ea ipsum excepteur dolor et laboris qui fugiat ad. Esse mollit exercitation do nulla officia reprehenderit. Ex et reprehenderit sunt pariatur nulla ipsum sunt minim aliquip labore reprehenderit ipsum proident in. Aliquip cillum anim deserunt excepteur duis culpa. Proident reprehenderit dolore magna laborum consequat. Ex exercitation aliquip duis enim aliqua irure sint tempor laborum eiusmod.</p>
+
+          <p>Quis aliqua cupidatat esse eiusmod. Ea sint et nostrud commodo veniam est esse qui elit ut tempor aute. Ad ad excepteur ullamco nulla aute et ipsum cillum sit elit. Eu dolor dolor fugiat esse non sunt minim.</p>
+
+          <p>Dolore nisi occaecat labore deserunt. Id enim proident incididunt quis magna sit cillum cillum eiusmod ex cillum duis ipsum. Esse consectetur reprehenderit sunt tempor magna nostrud exercitation do amet magna nostrud consequat labore.</p>
+        
+          <p>Do duis culpa qui elit voluptate laborum cillum veniam ad id velit aute. Qui fugiat laboris ad sunt. Sint enim adipisicing elit ullamco commodo sunt consectetur sit magna esse laboris voluptate ea enim. Elit et Lorem ullamco est excepteur pariatur adipisicing magna adipisicing cupidatat. Mollit ut tempor voluptate officia excepteur est do. Excepteur mollit in duis ad. Deserunt sint minim quis sit veniam.</p>
+
+          <p>Veniam officia id ex ipsum fugiat enim laboris. Eiusmod dolore eiusmod id laboris dolor laboris in sint ut amet. Sit non minim anim cupidatat. Eu ut quis Lorem cillum nulla laboris magna elit. Reprehenderit labore ut magna in adipisicing ea pariatur culpa.</p>
+        </div>
+      </div>
     </div>
   `;
 }
